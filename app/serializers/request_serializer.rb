5feedefa753a1b928bc92ac5880e7cb97a06d37c
vinyasbj <vinyas555@gmail.com>
2019-08-15 00:53:07 +0530

    new changes

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,6 @@
 class RequestSerializer < ActiveModel::Serializer
   attributes :id, :title, :name, :body, :headers , :collection_id , :url,:request_method, :request_params
+  
   def request_method
     object.method
   end
