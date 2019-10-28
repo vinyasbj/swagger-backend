795e2a2f9552fde62f39755c217495188181b0a0
vinyasbj <vinyas555@gmail.com>
2019-10-28 14:10:22 +0530

    added comments api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :requests, :defaults => { :format => 'json' }
   resources :collections, :defaults => { :format => 'json' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
