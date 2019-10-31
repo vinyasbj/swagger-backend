ffb10281ce9e1c310eca8bd8ebc7f97bf9680f0a
vinyasbj <vinyas555@gmail.com>
2019-10-31 22:32:41 +0530

    route error fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :comments, :defaults => { :format => 'json' }
   resources :requests, :defaults => { :format => 'json' }
   resources :collections, :defaults => { :format => 'json' }
-  get "request_comments",:to => "request_comments#comments"
+  get "request_comments",:to => "comments#request_comments"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
